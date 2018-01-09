9537db96bc5fd54d7ee561d3e1738ef058f340c2
Christoph Rumpel <c.rumpel@kabsi.at>
2018-01-09 17:51:52 +0100

    Fix missing apostrophe

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,7 @@
     <meta property="og:type"               content="article" />
     <meta property="og:title"              content="Hi I'm Christph, a web developer from Austria" />
     <meta property="og:description"        content="This is my personal blog where I write about PHP, Laravel, Git and Chatbots. I also talk about these topics." />
-    <meta property="og:image"              content="{{ asset('/images/cr_image.jpg') }} />
+    <meta property="og:image"              content="{{ asset('/images/cr_image.jpg') }}" />
 @endsection
 
 @section('content')

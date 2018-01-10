3ef1ca7f5ed51423dcaa75408dd56cce81a51b2c
Christoph Rumpel <c.rumpel@kabsi.at>
2018-01-10 12:38:35 +0100

    Add twitter meta tags

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,6 +6,12 @@
     <meta property="og:title"              content="Hi I'm Christoph, a web developer from Austria" />
     <meta property="og:description"        content="This is my personal blog where I write about PHP, Laravel, Git and Chatbots. I also talk about these topics. Checkout my site to find out more." />
     <meta property="og:image"              content="{{ asset('/images/cr_image_v2.jpg') }}" />
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@christophrumpel" />
+    <meta name="twitter:title" content="Hi I'm Christoph, a web developer from Austria" />
+    <meta name="twitter:description" content="This is my personal blog where I write about PHP, Laravel, Git and Chatbots. I also talk about these topics. Checkout my site to find out more." />
+    <meta name="twitter:image" content="{{ asset('/images/cr_image_v2.jpg') }}" />
 @endsection
 
 @section('content')
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,12 @@
     <meta property="og:title" content="{{ $post->title }}"/>
     <meta property="og:description" content="{{ strip_tags($post->summary) }}"/>
     <meta property="og:image" content="{{ $post->preview_image }}"/>
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@christophrumpel" />
+    <meta name="twitter:title" content="{{ $post->title }}" />
+    <meta name="twitter:description" content="{{ strip_tags($post->summary) }}" />
+    <meta name="twitter:image" content="{{ $post->preview_image }}" />
 @endsection
 
 @section('content')

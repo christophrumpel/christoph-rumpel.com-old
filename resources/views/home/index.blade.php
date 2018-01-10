1d2aa023bd80b2fc7cbaba3b45cb96c6f395649d
Christoph Rumpel <c.rumpel@kabsi.at>
2018-01-10 09:36:00 +0100

    rename image to flush cache

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,7 @@
     <meta property="og:type"               content="article" />
     <meta property="og:title"              content="Hi I'm Christoph, a web developer from Austria" />
     <meta property="og:description"        content="This is my personal blog where I write about PHP, Laravel, Git and Chatbots. I also talk about these topics. Checkout my site to find out more." />
-    <meta property="og:image"              content="{{ asset('/images/cr_image.jpg') }}" />
+    <meta property="og:image"              content="{{ asset('/images/cr_image_v2.jpg') }}" />
 @endsection
 
 @section('content')

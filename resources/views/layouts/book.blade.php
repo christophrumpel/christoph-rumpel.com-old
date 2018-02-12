90f35f024f68a97c583307be5447356e0dc0ebc5
Christoph Rumpel <c.rumpel@kabsi.at>
2018-02-12 12:48:47 +0100

    Add missing space

--- resources/views/layouts/book.blade.php
+++ resources/views/layouts/book.blade.php
@@ -8,14 +8,14 @@
     <meta property="og:type" content="article"/>
     <meta property="og:title" content="Build Chatbots with PHP"/>
     <meta property="og:description"
-          content="Finally a PHP chatbot starting point!Benefit from my experience and let me teach you everything you need to know to build chatbots in PHP."/>
+          content="Finally a PHP chatbot starting point! Benefit from my experience and let me teach you everything you need to know to build chatbots in PHP."/>
     <meta property="og:image" content="{{ asset('/images/book/book_ad.png') }}"/>
 
     <meta name="twitter:card" content="summary"/>
     <meta name="twitter:site" content="@christophrumpel"/>
     <meta name="twitter:title" content="Build Chatbots with PHP"/>
     <meta name="twitter:description"
-          content="Finally a PHP chatbot starting point!Benefit from my experience and let me teach you everything you need to know to build chatbots in PHP."/>
+          content="Finally a PHP chatbot starting point! Benefit from my experience and let me teach you everything you need to know to build chatbots in PHP."/>
     <meta name="twitter:image" content="{{ asset('/images/book/book_ad.png') }}"/>
 </head>
 <body class="font-sans">

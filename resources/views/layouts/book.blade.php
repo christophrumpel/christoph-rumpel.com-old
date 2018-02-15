6dcb1635b474e66671df482c02a71ac6b3b154c7
Christoph Rumpel <c.rumpel@kabsi.at>
2018-02-15 13:37:14 +0100

    Add fb button

--- resources/views/layouts/book.blade.php
+++ resources/views/layouts/book.blade.php
@@ -12,5 +12,25 @@
 @if(app()->environment('production'))
     @include('layouts.partials.analytics')
 @endif
+
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId            : 'your-app-id',
+            autoLogAppEvents : true,
+            xfbml            : true,
+            version          : 'v2.12'
+        });
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
 </body>
 </html>

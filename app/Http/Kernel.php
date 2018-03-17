4aa8885f6779daba971a27c77ceac51ce72ccd94
Christoph Rumpel <c.rumpel@kabsi.at>
2018-03-17 17:49:00 +0100

    Update nmp dependecnies and change middleware order

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,18 +32,11 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            // \App\Http\Middleware\EncryptCookies::class,
-            // \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            // \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            // \App\Http\Middleware\VerifyCsrfToken::class,
-            // \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            UpdateHeaderAndContentNonces::class,
+            \Spatie\ResponseCache\Middlewares\CacheResponse::class,
             \App\Http\Middleware\CacheControl::class,
             \App\Http\Middleware\Robots::class,
             SetReferrerPolicy::class,
-            UpdateHeaderAndContentNonces::class,
-            \Spatie\ResponseCache\Middlewares\CacheResponse::class,
             \Spatie\Csp\AddCspHeaders::class,
         ],
 

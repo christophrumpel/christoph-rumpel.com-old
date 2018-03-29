351a9a00009c19ddc0fdc62c6d006e2a8c1c8322
Christoph Rumpel <c.rumpel@kabsi.at>
2018-03-29 11:01:06 +0200

    Update book landing page title

--- resources/views/partials/book-header.blade.php
+++ resources/views/partials/book-header.blade.php
@@ -1,7 +1,7 @@
 <header class="book-bg w-screen flex justify-center items-center flex-col py-16 mb-32 relative">
     <div class="w-4/5 sm:w-3/5 md:w-100">
         <h1 id="headline" class="text-white m-0 mb-10 text-5xl font-landing text-center text-shadow-lighter">Finally a
-            starting point for your next PHP chatbot
+            starting point for your PHP chatbot
         </h1>
     </div>
     <div class="md:mt-10 flex w-screen justify-center items-center flex-row">

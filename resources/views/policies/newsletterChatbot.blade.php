e6b7cd610b61fb4a1cfdc937f674812be2c1e894
Christoph Rumpel <c.rumpel@kabsi.at>
2018-04-18 14:35:23 +0200

    Add processors to privacy policy

--- resources/views/policies/newsletterChatbot.blade.php
+++ resources/views/policies/newsletterChatbot.blade.php
@@ -32,9 +32,12 @@
         <ul>
             <li>Subscribe you</li>
             <li>Send you updates on the book Build Chatbots with PHP (Updates will be sent once or twice a month)</li>
-            <li>No other service is receiving this information</li>
         </ul>
-
+        <p>Who the data is shared with</p>
+        <ul>
+            <li>The application is deployed on servers from <a href="https://www.digitalocean.com/security/gdpr/">DigitalOcean</a> and <a href="https://all-inkl.com/datenschutz/">ALL-INKL.COM</a></li>
+            <li>No other service is receiving any personal data</li>
+        </ul>
         <h2>Security</h2>
 
         <p>We are committed to ensuring that your information is secure. In order to prevent unauthorised access or

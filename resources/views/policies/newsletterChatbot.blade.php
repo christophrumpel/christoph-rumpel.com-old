27ec381804a077e3a6e5440e1a00ada1a920ceef
Christoph Rumpel <c.rumpel@kabsi.at>
2018-04-20 13:06:51 +0200

    Update GDPR article due to feedback

--- resources/views/policies/newsletterChatbot.blade.php
+++ resources/views/policies/newsletterChatbot.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <article>
 
-        <h1>Business privacy policy - Build Chatbots with PHP Facebook Messenger Chatbot</h1>
+        <h1>Business Privacy Policy - Build Chatbots with PHP Facebook Messenger Chatbot</h1>
 
         <p>This privacy policy sets out how Christoph Rumpel uses and protects any information that you give Christoph
             Rumpel when you use the chatbot Build Chatbots with PHP.</p>
@@ -18,7 +18,7 @@
             from
             time to time to ensure that you are happy with any changes. This policy is effective from 25.05.2018.</p>
 
-        <h2>What we collect</h2>
+        <h2>What We Collect</h2>
 
         <p>We may collect the following information:</p>
         <ul>
@@ -26,7 +26,7 @@
             <li>Facebook Scoped User ID</li>
         </ul>
 
-        <h2>What we do with the information we gather</h2>
+        <h2>What We Do with the Information We Gather</h2>
 
         <p>We do need this information in order to:</p>
         <ul>
@@ -38,6 +38,8 @@
             <li>The application is deployed on servers from <a href="https://www.digitalocean.com/security/gdpr/">DigitalOcean</a> and <a href="https://all-inkl.com/datenschutz/">ALL-INKL.COM</a></li>
             <li>No other service is receiving any personal data</li>
         </ul>
+        <h2>How Long the Information Is Stored</h2>
+        <p>The information is stored as long as you are subscribed to the news. When you end the subscription, it will automatically be deleted.</p>
         <h2>Security</h2>
 
         <p>We are committed to ensuring that your information is secure. In order to prevent unauthorised access or
@@ -45,7 +47,7 @@
             secure
             the information we collect online.</p>
 
-        <h2>Controlling your personal information</h2>
+        <h2>Controlling Your Personal Information</h2>
 
         <p>You may choose to restrict the collection or use of your personal information in the following ways:</p>
         <ul>
@@ -71,7 +73,7 @@
             you
             updates on the book anymore.</p>
 
-        <h2>Our details</h2>
+        <h2>Our Details</h2>
         <p>This chatbot is owned and operated by Christoph Rumpel.
         <p>You can contact us at: c.rumpel@kabsi.at
 

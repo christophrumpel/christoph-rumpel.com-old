8e9b9848e9ba30b33ee99ce8dfb21e69a9d92a17
Christoph Rumpel <c.rumpel@kabsi.at>
2018-08-10 21:53:28 +0200

    Updat meta tags

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -2,7 +2,7 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
 <meta name="description"
-      content="Hi I am Christoph, a web developer from Austria. I blog and talk about PHP, Laravel, chatbots and more.">
+      content="Hi I am Christoph, a web developer and book author from Austria. I blog and talk about PHP, Laravel, chatbots, dotfiles, content security policy and more.">
 <meta name="author" content="Christoph Rumpel">
 @if(isset($canonical_url) && $canonical_url)
     <link rel="canonical" href="{{ $canonical_url }}">

f39113cc620e2a3b32649d0cfc0e72e50af85fea
Christoph Rumpel <c.rumpel@kabsi.at>
2018-08-12 14:12:59 +0200

    Update to Laravel 5.6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Paginator::useBootstrapThree();
     }
 
     /**

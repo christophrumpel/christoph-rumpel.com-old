8824a3ab2c3778cc10ad7ad7a293594e8db3cde7
Christoph Rumpel <c.rumpel@kabsi.at>
2019-05-30 17:02:58 +0200

    Add customizing nova article

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,18 @@ import Turbolinks from 'turbolinks';
 
 Turbolinks.start();
 
-document.addEventListener('turbolinks:load', () => {
-    Highlight.start();
-});
+document.addEventListener('turbolinks:click', function(e) {
+    if (e.target.getAttribute('href').charAt(0) === '#') {
+        return e.preventDefault()
+    }
+}, false);
+
+document.addEventListener('turbolinks:load', function(e) {
+    if (window.location.hash) {
+        // 1. You may also need to decodeURIComponent the hash if it contains certain characters.
+        // 2. Change query element to fit your situation
+        var element = document.getElementById(window.location.hash.substring(1));
+        var pos = element.getBoundingClientRect();
+        window.scrollTo(0, pos.y)
+    }
+}, false);

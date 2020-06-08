946a0f872d81bb2daab01bd891f4eea8acfc8fda
Christoph Rumpel <c.rumpel@kabsi.at>
2020-06-08 14:47:15 +0200

    Add more blog styles

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -3,7 +3,7 @@
 
         <time class="text-textBlue font-bold"
               datetime="{{ $post->date->format('Y-m-d') }}">{{ $post->date->format('F Y') }}</time>
-        <h1 class="font-display font-bold text-5xl mb-8 text-textBlue"><span class="underline--magical">{{$post->title}}</span></h1>
+        <h1 class="font-display font-bold text-5xl mb-8 text-textBlue"><span class="title-highlight-underline">{{$post->title}}</span></h1>
         <span class="border-black border-2 rounded p-2 uppercase">{{implode(', ', $post->categories)}}</span>
 
         {!! $post->content !!}

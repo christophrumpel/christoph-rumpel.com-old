396f5305f101bf370a93f4eee3e262f38f0722b1
Christoph Rumpel <c.rumpel@kabsi.at>
2020-06-09 16:46:51 +0200

    wip

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -3,8 +3,12 @@
 
         <time class="text-textBlue font-bold"
               datetime="{{ $post->date->format('Y-m-d') }}">{{ $post->date->format('F Y') }}</time>
-        <h1 class="font-display font-bold text-5xl mb-8 text-textBlue"><span class="title-highlight-underline">{{$post->title}}</span></h1>
-        <span class="border-black border-2 rounded p-2 uppercase">{{implode(', ', $post->categories)}}</span>
+        <h1 class="font-display font-bold text-5xl mb-8 text-textBlue"><span
+                class="title-highlight-underline">{{$post->title}}</span></h1>
+
+        @foreach($post->categories as $category)
+            <span class="border-black border-2 rounded p-2 uppercase">{{$category}}</span>
+        @endforeach
 
         <p>
             {!! $post->summary !!}

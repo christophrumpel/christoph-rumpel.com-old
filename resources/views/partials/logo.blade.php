08aa6a524b88ed865fd175efe93bf37b4e4473c9
Christoph Rumpel <c.rumpel@kabsi.at>
2020-07-02 15:01:29 +0200

    wip

--- resources/views/partials/logo.blade.php
+++ resources/views/partials/logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <div class="flex justify-center items-center rounded-full bg-highlightYellow w-10 h-10">CR</div>
+    <div class="flex justify-center items-center rounded-full bg-highlightYellow w-10 h-10 font-display font-bold">CR</div>
 </a>

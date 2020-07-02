d73f3dfacc6a40a4709fe432d5226316d49969a7
Christoph Rumpel <c.rumpel@kabsi.at>
2020-07-02 14:50:11 +0200

    wip

--- resources/views/livewire/postList.blade.php
+++ resources/views/livewire/postList.blade.php
@@ -1,19 +1,19 @@
 <div>
-    <div class="flex justify-between">
-        <h2 class="font-display text-2xl text-textBlue">Here is what I have been writing lately.</h2>
-        <input wire:model="searchTerm" type="text" placeholder="Search for..."
+    <div class="flex justify-between mb-16">
+        <h2 class="font-display text-4xl text-textBlue">Here are my latest blog posts:</h2>
+        <input wire:model="searchTerm" type="text" class="text-center rounded" placeholder="Search posts..."
                class="form-input relative rounded-md shadow-sm p-2"/>
     </div>
 
-    <ul>
+    <ul class="post-list">
         @foreach($results as $result)
-            <li class="my-4 p-8 border-textBlue border-t-2">
-
-                <h2 class="font-display text-3xl text-textBlue">
-                    <a class="block"
-                       href="{{$result->link()}}">{{ $result->title }}</a>
-                </h2>
-
+            <li class="my-4 border-textBlue border-t-2">
+                <a class="block p-8"
+                   href="{{$result->link()}}">
+                    <h2 class="font-display text-3xl text-textBlue">
+                        {{ $result->title }}
+                    </h2>
+                </a>
             </li>
         @endforeach
     </ul>

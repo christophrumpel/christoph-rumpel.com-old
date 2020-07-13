51ff44025b08eefdf2e7eafbb7f92fdae8917bf5
Christoph Rumpel <c.rumpel@kabsi.at>
2020-07-13 17:33:23 +0200

    Fix styles conflict

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -1,7 +1,7 @@
 <x-main-layout :post="$post">
-    <main class="post page mt-16">
+    <main class="mt-16">
 
-        <article>
+        <article class="post">
             <time class="text-textBlue font-bold"
                   datetime="{{ $post->date->format('Y-m-d') }}">{{ $post->date->format('F Y') }}</time>
             <h1 class="font-display font-bold text-3xl lg:text-5xl mb-8 text-textBlue"><span

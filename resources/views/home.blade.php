db1f7c9fc33b65a4201747b639425ccea2e01717
Christoph Rumpel <c.rumpel@kabsi.at>
2020-07-13 16:47:01 +0200

    wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,23 @@
-@extends('layouts.main')
+<x-main-layout>
+    <main class="post page mt-16">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">Dashboard</div>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                            You are logged in!
                         </div>
-                    @endif
-
-                    You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </main>
+</x-main-layout>

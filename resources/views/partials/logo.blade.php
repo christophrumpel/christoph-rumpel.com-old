f21be0fdedd92af2a08d0a943dcf9bffd2af83ef
Christoph Rumpel <c.rumpel@kabsi.at>
2020-07-14 13:35:30 +0200

    Outline logo to work on mobile as well

--- resources/views/partials/logo.blade.php
+++ resources/views/partials/logo.blade.php
@@ -1,4 +1,4 @@
-<a  href="/" class="block logo w-24 h-12 mb-8 sm:mb-12 lg:mb-0">
+<a  href="/" class="block logo mb-8 sm:mb-12 lg:mb-0">
     <div class="flex justify-center items-center font-display font-bold">
         @include('partials.svg.logo')
     </div>
---
+++ resources/views/partials/svg/logo_old.blade.php
@@ -0,0 +1,10 @@
+<svg class="overflow-visible" width="55px" height="50px" viewBox="0 0 43 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <title>CR</title>
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-family="Dirty-Brush, Dirty Brush" font-size="48" font-weight="normal">
+        <g id="1280x2048" transform="translate(-320.000000, -33.000000)" fill="#001D82">
+            <text id="CR">
+                <tspan x="321" y="68">CR</tspan>
+            </text>
+        </g>
+    </g>
+</svg>

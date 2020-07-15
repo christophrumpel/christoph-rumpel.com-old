ae3a57f3a50ff9614c893f565eb2252cb8f86c1a
Christoph Rumpel <c.rumpel@kabsi.at>
2020-07-15 22:20:18 +0200

    Change backup time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('mailcoach:delete-old-unconfirmed-subscribers')->daily();
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
         $schedule->command('backup:clean')->daily()->at('01:00');
-        $schedule->command('backup:run --only-db')->everyMinute();
+        $schedule->command('backup:run --only-db')->daily()->at('02:00');
     }
 
     /**
